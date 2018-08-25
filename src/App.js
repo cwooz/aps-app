86a59b65a99f5d6de032d4bf94b44e77bb7a40ec
chrisWoosley <woosley.christopher@gmail.com>
2018-08-25 01:32:33 -0400

    stopForNight

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
 import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
 
 class App extends Component {
   state = {
@@ -8,12 +11,49 @@ class App extends Component {
     title: ""
   };
 
+  handleChange = ({ target: { name, value } }) =>
+    this.setState({
+      [name]: value
+    });
+
+  handleCreate = e => {
+    e.preventDefault();
+    if (this.state.title) {
+      this.setState(({ items, title }) => ({
+        items: [
+          ...items,
+          {
+            title,
+            id: Date.now()
+          }
+        ],
+        title: ""
+      }));
+    }
+  };
+
   render() {
+    const { title } = this.state;
+
     return (
       <div className="App">
-        <Typography variant="display1" align="center" gutterBottom>
-          <h2>APS App</h2>
-        </Typography>
+        <Paper>
+          <Typography variant="display1" align="center" gutterBottom>
+            <h2>APS App</h2>
+          </Typography>
+          <form onSubmit={this.handleCreate}>
+            <TextField
+              name="title"
+              label="Item"
+              value={title}
+              onChange={this.handleChange}
+              margin="normal"
+            />
+            <Button type="submit" color="primary" variant="raised">
+              Create
+            </Button>
+          </form>
+        </Paper>
       </div>
     );
   }
